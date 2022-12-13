91488a14b2482fa60f9c3b3d8ed1b95f26b10b43
jdh7 <john.daniel.hill@gmail.com>
2022-12-13 11:05:56 -0500

    day 3 start

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -2,9 +2,21 @@ def get_input():
     db = []
     with open(r"D:\Coding\AdventofCode2022\Day3\31.in", "r") as f:
         for line in f.readlines():
-            db.append([c for c in line.strip().split()])
+            db.append(line.strip().split())
     return db
 
 
 x = get_input()
-print(x)
+
+
+# split contents into first and second compartment
+
+i = x[0][0]
+first_half = i[: int(len(i) / 2)]
+second_half = i[int(len(i) / 2) :]
+
+# assign priorities
+items = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+for i in items:
+    print(i)
